f2245c3461b009f96068a08476d396c6bdb720cf
Ggabriel22 <bolanosgabriel2217@gmail.com>
2022-05-31 20:31:58 -0600

    Primer commit

---
+++ LoginSimple/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LoginSimple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BoxUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BoxPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BoxUser
+            // 
+            this.BoxUser.Location = new System.Drawing.Point(185, 63);
+            this.BoxUser.Name = "BoxUser";
+            this.BoxUser.Size = new System.Drawing.Size(188, 20);
+            this.BoxUser.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingresa  usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingresa Password:";
+            // 
+            // BoxPassword
+            // 
+            this.BoxPassword.Location = new System.Drawing.Point(185, 124);
+            this.BoxPassword.Name = "BoxPassword";
+            this.BoxPassword.PasswordChar = '*';
+            this.BoxPassword.Size = new System.Drawing.Size(188, 20);
+            this.BoxPassword.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(298, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Location = new System.Drawing.Point(185, 160);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(82, 21);
+            this.btnRegistro.TabIndex = 5;
+            this.btnRegistro.Text = "Registrar";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 247);
+            this.Controls.Add(this.btnRegistro);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BoxPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BoxUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox BoxUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox BoxPassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRegistro;
+    }
+}
+
---
+++ LoginSimple/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string usuario = "nekszer";
+        string contraseña = "nekszer";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          string user =  BoxUser.Text;
+          string password = BoxPassword.Text;
+            if (user.Equals(usuario))
+            { 
+                if (password.Equals(contraseña))
+                {
+                    MessageBox.Show("Login exitoso");
+                }
+                else
+                {
+                    MessageBox.Show("La contraseña es incorrecta");
+                    //Falla por contraseña
+                }
+            } 
+            else
+            {
+                MessageBox.Show("No se reconoce usuario");
+                //Fall por usuario
+            }
+
+        
+
+        }
+
+        private void btnRegistro_Click(object sender, EventArgs e)
+        {
+            
+                
+            
+
+        }
+
+    }
+}
